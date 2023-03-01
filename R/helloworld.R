caf2751e0421065c68e350008ff2bf1cf1800ddd
Reshma Kassanjee <28223012+rkassanjee@users.noreply.github.com>
2023-03-01 16:17:07 +0200

    initial commit

---
+++ R/helloworld.R
@@ -0,0 +1,10 @@
+#' Say hello to the world
+#'
+#' @return Just displays something to the screen
+#' @export
+#'
+#' @examples
+#' helloworld()
+helloworld <- function(){
+  print('hello crazy world')
+}
